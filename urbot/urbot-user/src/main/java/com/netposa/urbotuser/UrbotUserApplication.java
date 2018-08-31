761d6817433077e0e6bb6a0d188bd2287209c73d
cyy314 <mrcai0413@gmail.com>
2018-08-31 23:29:39 +0800

    https http

--- urbot/urbot-user/src/main/java/com/netposa/urbotuser/UrbotUserApplication.java
+++ urbot/urbot-user/src/main/java/com/netposa/urbotuser/UrbotUserApplication.java
@@ -1,8 +1,17 @@
 package com.netposa.urbotuser;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerFactory;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -16,6 +25,70 @@ public class UrbotUserApplication {
 			httpSecurity.csrf().disable();
 		}
 	}*/
+	
+	/**
+     * 配置一个TomcatEmbeddedServletContainerFactory bean
+     * @return
+     */
+    /*@Bean
+    public EmbeddedServletContainerFactory servletContainer() {
+
+        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
+
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection collection = new SecurityCollection();
+                collection.addPattern("/*");
+                securityConstraint.addCollection(collection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        tomcat.addAdditionalTomcatConnectors(initiateHttpConnector());
+        return tomcat;
+    }*/
+
+    /**
+     * 让我们的应用支持HTTP是个好想法，但是需要重定向到HTTPS，
+     * 但是不能同时在application.properties中同时配置两个connector，
+     * 所以要以编程的方式配置HTTP connector，然后重定向到HTTPS connector
+     * @return Connector
+     */
+    /*private Connector initiateHttpConnector() {
+    	//http端口跳转至https接口
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        //Connector监听的http的端口号
+        connector.setPort(9090);
+        connector.setSecure(false);
+        //监听到http的端口号后转向到的https的端口号
+        connector.setRedirectPort(8763);
+    	//设置http接口
+    	Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+    	connector.setPort(9091);
+        return connector;
+    }*/
+    
+	/**
+	 * 同时支持https和http
+	 * @return
+	 */
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {          
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				if (container instanceof TomcatEmbeddedServletContainerFactory) {
+                    TomcatEmbeddedServletContainerFactory containerFactory =
+                            (TomcatEmbeddedServletContainerFactory) container;
+                    Connector connector = new Connector(TomcatEmbeddedServletContainerFactory.DEFAULT_PROTOCOL);
+                    connector.setPort(8080);
+                    containerFactory.addAdditionalTomcatConnectors(connector);
+                }
+			}
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(UrbotUserApplication.class, args);
